3369c1208996840fecf9aa8f1e6f7a7cb949539d
X-Yin <XY18833217116@163.com>
2021-08-15 16:33:43 +0800

    feat: 定义 ast 接口标准

--- src/core/compile/index.js
+++ src/core/compile/index.js
@@ -1,5 +1,14 @@
 /**
  * html to ast
  * ast 定义标准
- * {  }
+ * {
+ *     attrs: [{name 'id', value: 'app'}],
+ *     children: [{...}],
+ *     parent: [{...}] || null,
+ *     tag: 'div',
+ *     staticClass: "\"container\""
+ *     staticStyle: "{\"color\":\"red\"}"
+ *     events: [{click: 'clickHandler', 'doubleClick': 'handler'}]
+ *     type: 1 https://developer.mozilla.org/zh-CN/docs/Web/API/Node/nodeType
+ * }
  * */
