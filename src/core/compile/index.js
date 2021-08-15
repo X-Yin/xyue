63e443712ddbdea603d2d68319d7a32c8fde971d
X-Yin <XY18833217116@163.com>
2021-08-15 16:35:30 +0800

    feat: 初始化 compile

--- src/core/compile/index.js
+++ src/core/compile/index.js
@@ -12,3 +12,14 @@
  *     type: 1 https://developer.mozilla.org/zh-CN/docs/Web/API/Node/nodeType
  * }
  * */
+
+
+/**
+ * template html 字符串
+ * options 可选参数
+ * */
+export default function parse(template, options) {
+    const ast = {};
+    // ...
+    return ast;
+}
